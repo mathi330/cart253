1fecee4982b479f31170417c228631dd9b58ca57
mathi330 <mathilded@protonmail.com>
2021-09-09 16:13:45 -0400

    drawing experiment: modification of colors

--- experiments/drawing-experiment/js/script.js
+++ experiments/drawing-experiment/js/script.js
@@ -4,7 +4,7 @@
 Template p5 project
 Pippin Barr
 
-Here is a description of this template p5 project.
+Playing around with colors and shapes.
 **************************************************/
 
 // setup()
@@ -17,23 +17,23 @@ function setup() {
   background(200, 0, 100);
 
   noStroke(1);
-  fill(210, 0, 100);
+  fill(210, 0, 100, 100);
   rotate(PI / 100.0);
   rect(20, 0, 880, 480, 0, 25, 0, 0);
 
-  fill(220, 0, 100);
+  fill(220, 0, 100, 100);
   rotate(PI / 90.0);
   rect(20, 0, 880, 480, 0, 25, 0, 0);
 
-  fill(230, 0, 100);
+  fill(230, 0, 100, 100);
   rotate(PI / 80.0);
   rect(20, 0, 880, 480, 0, 25, 0, 0);
 
-  fill(240, 0, 100);
+  fill(240, 0, 100, 100);
   rotate(PI / 70.0);
   rect(20, 0, 880, 480, 0, 25, 0, 0);
 
-  fill(250, 0, 100);
+  fill(250, 0, 100, 100);
   rotate(PI / 60.0);
   rect(20, 0, 880, 480, 0, 25, 0, 0);
 }
@@ -42,14 +42,21 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  stroke(255);
+  stroke(0, 0, 0, 10);
+  noFill();
   triangle(width/2, height/2, width/2 + 69, height/2, width/2, height/2 + 69);
 
+  noStroke();
+  fill(204, 204, 255, 120);
   ellipse(width/2, height/2, 40, 40);
+  fill(195, 195, 246, 120);
   ellipse(width/2, height/2, 30, 30);
+  fill(186, 186, 237, 120);
   ellipse(width/2, height/2, 20, 20);
+  fill(177, 177, 228, 120);
   ellipse(width/2, height/2, 10, 10);
 
+  stroke(255);
   strokeWeight(2);
   line(0, 0, width/6, height/3);
   line(width/6, height/3, width/4, height/5);
