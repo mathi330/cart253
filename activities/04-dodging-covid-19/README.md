570e5536dcf1996ea59e69f6ff77e3838dc57bb2
mathi330 <mathilded@protonmail.com>
2021-09-26 22:28:32 -0400

    A4: Make the program stop when user and covid virus collide, added static effect + comments

--- activities/04-dodging-covid-19/README.md
+++ activities/04-dodging-covid-19/README.md
@@ -1,3 +1,3 @@
 # Dodging COVID-19
 
-Blah, blah, blah...
+This project is a covid-19 simulation where the user (white) needs to avoid the virus (red), otherwise the program stops. The user can interact with the program through the mouse.
--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -2,18 +2,22 @@
 Dodging COVID-19
 Mathilde Davan
 
-Blah, blah, blah...
+This project is a covid-19 simulation where the user (white) needs to avoid the virus (red), otherwise the program stops.
 */
 
 "use strict";
 
+//background static.
+let staticAmount = 1000;
+
+//Object for the covid-19 virus.
 let covid = {
   x: 0,
   y: 250,
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5,
+  speed: 10,
   fill: {
     r: 255,
     g: 0,
@@ -21,37 +25,51 @@ let covid = {
   },
 };
 
+//Object for the user.
 let user = {
   x: undefined,
   y: undefined,
-  size: 80,
+  size: 100,
   fill: 255,
 };
 
 /**
-Description of preload
-*/
-function preload() {}
+setup()
 
-/**
-Description of setup
+Create canvas,
+determine Covid-19's y position at the beginning of the program and its speed to move,
+erase the cursor on the canvas.
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
   covid.y = random(0, height);
   covid.vx += covid.speed;
+  noCursor();
 }
 
 /**
-Description of draw()
+draw()
+
+Create the background (color + static),
+make the covid simulation,
+create the user's simulation,
+determine the distance between the user and covid to know if they touch (to see if the user is contaminated).
 */
 function draw() {
   background(0);
+  //Create the static effect.
+  for (let i = 0; i < staticAmount; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(255);
+    point(x, y);
+  }
 
+  //Determine the coordinates of the circle representing he user.
   user.x = mouseX;
   user.y = mouseY;
 
-  //Covid-19
+  //Create the covid-19 simulation.
   push();
   fill(covid.fill.r, covid.fill.g, covid.fill.b);
   noStroke();
@@ -60,15 +78,22 @@ function draw() {
   covid.y += covid.vy;
   ellipse(covid.x, covid.y, covid.size);
   pop();
-
+  //If statement to change the y coordinates everytime x goes back to 0.
   if (covid.x >= width) {
     covid.x = 0;
     covid.y = random(0, height);
   }
 
-  //user
+  //Create the user's simulation.
   push();
   fill(user.fill);
   ellipse(user.x, user.y, user.size);
   pop();
+
+  //Evaluating the distance between the covid and the user.
+  let d = dist(user.x, user.y, covid.x, covid.y);
+
+  if (d <= covid.size / 2 + user.size / 2) {
+    noLoop();
+  }
 }
