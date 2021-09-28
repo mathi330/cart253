e3b4125791521b3fc46761420c691611d1d8d90f
mathi330 <mathilded@protonmail.com>
2021-09-28 11:31:14 -0400

    Ex: Create 3 enemies moving on the canvas

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Dodge 'em
+Mathilde Davan
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,164 @@ author, and this description to match your project!
 
 "use strict";
 
+let bg = {
+  r: 50,
+  g: 50,
+  b: 50,
+  a: 100,
+};
+
+let enemy1 = {
+  x: 0,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speedx: 4.3,
+  speedy: 1,
+  color: {
+    r: 200,
+    g: 100,
+    b: 150,
+  },
+};
+
+let enemy2 = {
+  x: 0,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speedx: 1,
+  speedy: 4,
+  color: {
+    r: 200,
+    g: 100,
+    b: 150,
+  },
+};
+
+let enemy3 = {
+  x: 0,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speedx: -3,
+  speedy: 1,
+  color: {
+    r: 200,
+    g: 100,
+    b: 150,
+  },
+};
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  enemy1.x = random(0, width);
+  enemy1.y = enemy1.size / 2;
+  enemy1.speedy = random(1, 4);
 
+  enemy2.y = random(0, height);
+  enemy2.x = enemy2.size / 2;
+  enemy2.speedx = random(1, 4);
+
+  enemy3.x = random(0, width);
+  enemy3.y = enemy3.size / 2;
+  enemy3.speedy = random(1, 4);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(bg.r, bg.g, bg.b, bg.a);
 
+  //Create 3 enemies
+  // Enemy 3.
+  noStroke();
+  fill(enemy1.color.r, enemy1.color.g, enemy1.color.b);
+  //Setting the trajectory of enemy 1.
+  enemy1.vx = enemy1.speedx;
+  enemy1.vy = enemy1.speedy;
+  enemy1.x += enemy1.vx;
+  enemy1.y += enemy1.vy;
+  ellipse(enemy1.x, enemy1.y, enemy1.size);
+  // Make the enemy change direction when touching the sides of the canvas.
+  if (enemy1.x >= width - enemy1.size / 2 || enemy1.x <= 0 + enemy1.size / 2) {
+    enemy1.speedx = -enemy1.speedx;
+  }
+  if (enemy1.y >= height - enemy1.size / 2 || enemy1.y <= 0 + enemy1.size / 2) {
+    enemy1.speedy = -enemy1.speedy;
+  }
+
+  // Enemy 2
+  noStroke();
+  fill(enemy2.color.r, enemy2.color.g, enemy2.color.b);
+  //Setting the trajectory of enemy 1.
+  enemy2.vx = enemy2.speedx;
+  enemy2.vy = enemy2.speedy;
+  enemy2.x += enemy2.vx;
+  enemy2.y += enemy2.vy;
+  ellipse(enemy2.x, enemy2.y, enemy2.size);
+  // Make the enemy change direction when touching the sides of the canvas.
+  if (enemy2.x >= width - enemy2.size / 2 || enemy2.x <= 0 + enemy2.size / 2) {
+    enemy2.speedx = -enemy2.speedx;
+  }
+  if (enemy2.y >= height - enemy2.size / 2 || enemy2.y <= 0 + enemy2.size / 2) {
+    enemy2.speedy = -enemy2.speedy;
+  }
+
+  // Enemy 3.
+  noStroke();
+  fill(enemy3.color.r, enemy3.color.g, enemy3.color.b);
+  //Setting the trajectory of enemy 1.
+  enemy3.vx = enemy3.speedx;
+  enemy3.vy = enemy3.speedy;
+  enemy3.x += enemy3.vx;
+  enemy3.y += enemy3.vy;
+  ellipse(enemy3.x, enemy3.y, enemy3.size);
+  // Make the enemy change direction when touching the sides of the canvas.
+  if (enemy3.x >= width - enemy3.size / 2 || enemy3.x <= 0 + enemy3.size / 2) {
+    enemy3.speedx = -enemy3.speedx;
+  }
+  if (enemy3.y >= height - enemy3.size / 2 || enemy3.y <= 0 + enemy3.size / 2) {
+    enemy3.speedy = -enemy3.speedy;
+  }
+
+  //Make the enemies change direction when they collide together.
+  //Collision between enemies 1 and 2.
+  let d1 = dist(enemy1.x, enemy1.y, enemy2.x, enemy2.y);
+  if (d1 <= enemy1.size / 2 + enemy2.size / 2) {
+    enemy1.speedx = -enemy1.speedx;
+    enemy1.speedy = -enemy1.speedy;
+    enemy2.speedx = -enemy2.speedx;
+    enemy2.speedy = -enemy2.speedy;
+  }
+
+  //Collision between enemies 1 and 3.
+  let d2 = dist(enemy1.x, enemy1.y, enemy3.x, enemy3.y);
+  if (d2 <= enemy1.size / 2 + enemy3.size / 2) {
+    enemy1.speedx = -enemy1.speedx;
+    enemy1.speedy = -enemy1.speedy;
+    enemy3.speedx = -enemy3.speedx;
+    enemy3.speedy = -enemy3.speedy;
+  }
+
+  //Collision between enemies 2 and 3.
+  let d3 = dist(enemy2.x, enemy2.y, enemy3.x, enemy3.y);
+  if (d3 <= enemy2.size / 2 + enemy3.size / 2) {
+    enemy2.speedx = -enemy2.speedx;
+    enemy2.speedy = -enemy2.speedy;
+    enemy3.speedx = -enemy3.speedx;
+    enemy3.speedy = -enemy3.speedy;
+  }
 }
