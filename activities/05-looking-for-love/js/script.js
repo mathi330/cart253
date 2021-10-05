2de5f1ae3cc11833560d09c949248506a7021bee
mathi330 <mathilded@protonmail.com>
2021-10-05 10:11:11 -0400

    A5: Finished!

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -8,6 +8,26 @@ author, and this description to match your project!
 
 "use strict";
 
+let circle1 = {
+  x: undefined,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
+
+let circle2 = {
+  x: undefined,
+  y: undefined,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+};
+
+let state = `title`;
+
 /**
 Description of preload
 */
@@ -16,9 +36,111 @@ function preload() {}
 /**
 Description of setup
 */
-function setup() {}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  setupCircles();
+}
+
+function setupCircles() {
+  // positioning the circles.
+  circle1.x = width / 3;
+  circle1.y = height / 2;
+  circle2.x = (width / 3) * 2;
+  circle2.y = height / 2;
+
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle1.vy = random(-circle1.speed, circle1.speed);
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
+}
 
 /**
 Description of draw()
 */
-function draw() {}
+function draw() {
+  background(0);
+
+  if (state === `title`) {
+    title();
+  } else if (state === `simulation`) {
+    simulation();
+  } else if (state === `love`) {
+    love();
+  } else if (state === `sadness`) {
+    sadness();
+  }
+}
+
+function title() {
+  push();
+  textSize(64);
+  fill(200, 100, 100);
+  textAlign(CENTER, CENTER);
+  text(`LOVE?`, width / 2, height / 2);
+  pop();
+}
+
+function simulation() {
+  move();
+  checkOffScreen();
+  checkOverlap();
+  display();
+}
+
+function love() {
+  push();
+  textSize(64);
+  fill(255, 150, 150);
+  textAlign(CENTER, CENTER);
+  text(`LOVE!`, width / 2, height / 2);
+  pop();
+}
+
+function sadness() {
+  push();
+  textSize(64);
+  fill(150, 150, 255);
+  textAlign(CENTER, CENTER);
+  text(`:(`, width / 2, height / 2);
+  pop();
+}
+
+function move() {
+  circle1.x += circle1.vx;
+  circle1.y += circle1.vy;
+
+  circle2.x += circle2.vx;
+  circle2.y += circle2.vy;
+}
+
+function checkOffScreen() {
+  if (isOffScreen(circle1) || isOffScreen(circle2)) {
+    state = `sadness`;
+  }
+}
+
+function isOffScreen(circle) {
+  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkOverlap() {
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size / 2 + circle2.size / 2) {
+    state = `love`;
+  }
+}
+
+function display() {
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
+}
