ded4ca7ebb565ec8ffd65a5ffdb6dab3a9d97b25
mathi330 <mathilded@protonmail.com>
2021-10-05 09:21:47 -0400

    A5: Start looking for love

---
+++ activities/05-looking-for-love/js/script.js
@@ -0,0 +1,24 @@
+/**
+Looking for Love
+Mathilde Davan
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {}
+
+/**
+Description of draw()
+*/
+function draw() {}
