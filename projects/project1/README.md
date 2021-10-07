337f0745de701c08622e6e5f936443da24470c30
mathi330 <mathilded@protonmail.com>
2021-10-07 16:09:15 -0400

    P1: Created ground and material

--- projects/project1/README.md
+++ projects/project1/README.md
@@ -1,3 +1,8 @@
 # Template p5 project
 
 This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+
+What I want to do:
+Some kind of game where the goal is to use "material" to protect something (not sure what yet) from "enemies."
+If the thing that is being protected is touched by anything, "material" or "enemies," it dies.
+"enemies" disappear when they touch "material" and "material" looses some "durability" (represented by the opacity).
--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -8,26 +8,120 @@ author, and this description to match your project!
 
 "use strict";
 
+let ground = {
+  x: undefined,
+  y: undefined,
+  sizeX: undefined,
+  sizeY: 50,
+  fill: {
+    r: 200,
+    g: 200,
+    b: 200,
+  },
+};
+
+let materials = {
+  x: undefined,
+  y: undefined,
+  sizeX: undefined,
+  minSizeX: undefined,
+  maxSizeX: undefined,
+  sizeY: undefined,
+  minSizeY: undefined,
+  maxSizeY: undefined,
+  isBeingDragged: false,
+
+  fill: {
+    r: 250,
+    g: 50,
+    b: 50,
+  },
+  stroke: {
+    r: 0,
+    g: 0,
+    b: 0,
+    strokeWeight: 3,
+  },
+};
 
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(1000, 700);
+  rectMode(CENTER);
 
+  setupGround();
+  setupMaterial();
 }
 
+/**
+setupGround()
+
+function to set up the grounds size and coordinates.
+*/
+function setupGround() {
+  //determining the height and lenght of the ground depending on the size of the canvas.
+  ground.sizeX = width;
+  ground.sizeY = height / 11;
+  //placing the ground according to the size of the canvas and size of the ground.
+  ground.x = width / 2;
+  ground.y = height - ground.sizeY / 2;
+}
+
+/**
+setupMaterial()
+
+function that sets up size and coordinates for material.
+*/
+function setupMaterial() {
+  //choosing random sizes for the material.
+  materials.sizeX = random(width / 15, width / 8);
+  materials.sizeY = random(height / 15, height / 11);
+  //choosing coordiantes of the material while taking into account the ground and the material's size.
+  materials.x = random(0 + materials.sizeX / 2, width - materials.sizeX / 2);
+  materials.y = ground.y - ground.sizeY / 2 - materials.sizeY / 2;
+  // - materials.stroke.strokeWeight;
+}
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
+  displayGround();
 
+  displayMaterial();
+}
+
+/**
+displayGround()
+
+function that displays the ground/lower part of the canvas.
+*/
+function displayGround() {
+  push();
+  noStroke();
+  fill(ground.fill.r, ground.fill.g, ground.fill.b);
+  rect(ground.x, ground.y, ground.sizeX, ground.sizeY);
+  pop();
+}
+
+/**
+displayMaterial()
+
+function that displays the material.
+*/
+function displayMaterial() {
+  //fill and stroke (for now I don't have a stroke but I still put it in case I change my mind).
+  fill(materials.fill.r, materials.fill.g, materials.fill.b);
+  noStroke();
+  // strokeWeight(materials.stroke.strokeWeight);
+  // stroke(materials.stroke.r, materials.stroke.g, materials.stroke.b);
+  rect(materials.x, materials.y, materials.sizeX, materials.sizeY);
 }
