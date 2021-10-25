64579a31816629cb1c287e1268bb6bc9bfd9602a
mathi330 <mathilded@protonmail.com>
2021-10-25 17:47:20 -0400

    E5: Activity code

--- exercises/juggle-garden/js/script.js
+++ exercises/juggle-garden/js/script.js
@@ -8,17 +8,39 @@ author, and this description to match your project!
 
 "use strict";
 
-/**
-Description of preload
-*/
-function preload() {}
+let gravityForce = 0.0025;
 
-/**
-Description of setup
-*/
-function setup() {}
+let paddle;
 
-/**
-Description of draw()
-*/
-function draw() {}
+let balls = [];
+let numBalls = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
+}
+
+function draw() {
+  background(0);
+
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
+}
