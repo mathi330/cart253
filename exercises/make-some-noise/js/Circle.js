02b1f4a81ecfee1cb5dee085a7a6eb950bf51650
mathi330 <mathilded@protonmail.com>
2021-11-12 17:03:37 -0500

    E6: Make a new circle appear when the mouse is pressed and the sound start and stop with spacebar, changed the frequency range to add some variety in the soundscape, added more comments

--- exercises/make-some-noise/js/Circle.js
+++ exercises/make-some-noise/js/Circle.js
@@ -1,5 +1,5 @@
 class Circle {
-  constructor(start) {
+  constructor(start, sound) {
     this.x = random(0, width);
     this.y = random(0, height);
     this.size = random(20, 100);
@@ -16,8 +16,12 @@ class Circle {
     this.t = 1000 * start;
     this.speed = 0.003;
 
-    this.freq = random(20, 800);
-    this.amp = 0.2;
+    //2 possible ranges of frequency
+    this.freqRange1 = [-50, 440];
+    this.freqRange2 = [440, 840];
+    this.freqRange3 = [20, 940];
+    this.freq = random(20, 840); //initial frequency
+    this.amp = 0.1;
 
     //envelop test (taken from the p5.js envelop library: https://p5js.org/reference/#/p5.Envelope)
     this.t1 = random(10); // attack time in seconds
@@ -28,39 +32,51 @@ class Circle {
     this.reverbTime = random(0.5, 15);
     this.reverbDecay = random(0.0, 10);
     this.oscillator = undefined;
-    this.playingSound = false;
+    this.playingSound = sound; //Sees if the sound is playing
   }
 
+  //Makes the circle move using noise
   move() {
     this.x = map(noise(this.t), 0, 1, -100, width + 100);
     this.y = map(noise(this.t + 200), 0, 1, -100, height + 100);
     this.t += this.speed;
   }
 
+  //Chooses the range of the sound associated with the circle's coordinates
   sound() {
-    this.freq = map(this.x, 0, width, 20, 990);
-    this.amp = map(this.y, 0, height, 0, 0.2);
+    let myFreq = random(this.freqRange1, this.freqRange2, this.freqRange3); //Chooses the range of the frequence for the circle
+    //Applies the reange of the frequence according to the range chosen above
+    if (myFreq === this.freqRange1) {
+      this.freq = map(this.x, 0, width, this.freqRange1[0], this.freqRange1[1]);
+    } else if (myFreq === this.freqRange2) {
+      this.freq = map(this.x, 0, width, this.freqRange2[0], this.freqRange2[1]);
+    } else if (myFreq === this.freqRange3) {
+      this.freq = map(this.x, 0, width, this.freqRange3[0], this.freqRange3[1]);
+    }
+
+    this.amp = map(this.y, 0, height, 0.1, 0); //Louder when the circle is at the top and quieter when the circle is closer to the bottom of the canvas
   }
 
+  //Make the color and size of the circle change by using the audio input from a mic
   colorChange() {
     let micLevel = mic.getLevel();
 
     //size
     let newSize = this.size + micLevel * 10;
-    newSize = constrain(newSize, this.minSize, this.maxSize);
+    newSize = constrain(newSize, this.minSize, this.maxSize); //Stops the circle from becoming too big or too small.
     this.size = newSize;
 
-    //color
+    //color => becomes more red/pink
     let red = this.stroke.r + micLevel * 255;
     this.stroke.r = red;
   }
 
+  //Creates and displays the circle
   display() {
     push();
     noFill();
     strokeWeight(this.strokeWeight);
     stroke(this.stroke.r, this.stroke.g, this.stroke.b, this.stroke.a);
-
     ellipse(this.x, this.y, this.size);
     pop();
   }
--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Make Some Noise
+Mathilde Davan
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -10,24 +10,92 @@ author, and this description to match your project!
 
 let mic;
 
-let circles = [];
-let numCircles = 10;
-
 let reverb;
 
+let circles = [];
+let numCircles = 1;
+
+let playingSound = false;
+
 /**
-Description of setup
+setup()
+
+Setup the circle and sound information necessary in the program
 */
 function setup() {
   createCanvas(700, 700);
   userStartAudio();
 
+  //Call Reverb and AudioIn to use them in the program
   reverb = new p5.Reverb();
   mic = new p5.AudioIn();
   mic.start();
 
+  //Create the original circle(s) that are present at the beginning of the program
   for (let i = 0; i < numCircles; i++) {
-    let circle = new Circle(i);
+    let circle = new Circle(i, false); //create the circle
+
+    circle.oscillator = new p5.Envelope(
+      circle.t1,
+      circle.l1,
+      circle.t2,
+      circle.l2
+    ); //create an envelop for the sound
+    circle.oscillator = new p5.Oscillator(circle.freq, `sine`); //add/associate a sound to the circle
+    circles.push(circle); //put the newly created circle in the circles array
+
+    reverb.process(circle.oscillator, 3, 2); //Creates a reverb effect for the created circle
+  }
+}
+
+/**
+draw()
+
+Display the circles and play the sound when necessary
+*/
+function draw() {
+  background(0, 100);
+
+  //Display the circles
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    //call the functions from the circle class
+    circle.move();
+    circle.sound();
+    circle.colorChange();
+    circle.display();
+
+    //add sound to the circle using the information from the circle class
+    circle.oscillator.freq(circle.freq);
+    circle.oscillator.amp(circle.amp);
+  }
+}
+
+/**
+mousePressed()
+
+Add a circle every time the mouse is pressed
+*/
+function mousePressed() {
+  //If statement to determine wether the sound associated with the new circle should be heard or not
+  //If the other circles' sound are not playing
+  if (playingSound === false) {
+    let circle = new Circle(circles.length, false); //create a new circle
+
+    circle.oscillator = new p5.Envelope(
+      circle.t1,
+      circle.l1,
+      circle.t2,
+      circle.l2
+    ); //create an envelop for the sound of the new circle
+    circle.oscillator = new p5.Oscillator(circle.freq, `sine`); //create a new oscillator for the new circle
+    circles.push(circle); //adds the circle to the array of already existing circles
+
+    reverb.process(circle.oscillator, 3, 2); //adds a reverb effect to the sound
+
+    //If the other circles' sound are playing
+  } else if (playingSound === true) {
+    let circle = new Circle(circles.length, true);
 
     circle.oscillator = new p5.Envelope(
       circle.t1,
@@ -39,37 +107,35 @@ function setup() {
     circles.push(circle);
 
     reverb.process(circle.oscillator, 3, 2);
+    circle.oscillator.start(); //starts the sound for this circle too
   }
 }
 
 /**
-Description of draw()
+keyPressed()
+
+Make the sound start and stop playing when pressing the spacebar.
 */
-function draw() {
-  background(0, 100);
+function keyPressed() {
+  //Sees if the key that is pressed is the spacebar
+  if (keyCode === 32) {
+    for (let i = 0; i < circles.length; i++) {
+      let circle = circles[i];
 
-  for (let i = 0; i < circles.length; i++) {
-    let circle = circles[i];
-    circle.move();
-    circle.sound();
-    circle.colorChange();
-    circle.display();
+      //If the playingSound variable is false (the sound is not playing)
+      if (circle.playingSound === false) {
+        circle.oscillator.start(); //Start the sound
+        //Set the playingSound variables to true
+        circle.playingSound = true;
+        playingSound = true;
 
-    circle.oscillator.freq(circle.freq);
-    circle.oscillator.amp(circle.amp);
-  }
-}
-
-// Make the sound start and stop playing when clicking on the canvas.
-function mousePressed() {
-  for (let i = 0; i < circles.length; i++) {
-    let circle = circles[i];
-    if (circle.playingSound === false) {
-      circle.oscillator.start();
-      circle.playingSound = true;
-    } else if (circle.playingSound === true) {
-      circle.oscillator.stop();
-      circle.playingSound = false;
+        //If the playingSound variable is true (the sound is playing)
+      } else if (circle.playingSound === true) {
+        circle.oscillator.stop(); //Stop the sound
+        //Set the playingSound variables to false
+        circle.playingSound = false;
+        playingSound = false;
+      }
     }
   }
 }
